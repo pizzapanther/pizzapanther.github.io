d4df87ceceadd84ba4f42df44805fe889568a79b
Paul Bailey <paul.m.bailey@gmail.com>
2017-06-04 21:48:42 +0000

    add tests

---
+++ libs/djzen/tests/test_project.py
@@ -0,0 +1,38 @@
+import os
+import shutil
+import subprocess
+from unittest import TestCase
+
+class Project (TestCase):
+  def setUp (self):
+    self.name = 'testproj'
+    self.files = [
+      '.env',
+      '.pipconfig',
+      '.gitignore',
+      'manage.py',
+      os.path.join(self.name, 'urls.py'),
+      os.path.join(self.name, 'wsgi.py'),
+      os.path.join(self.name, 'settings', 'production.py'),
+      os.path.join(self.name, 'settings', 'development.py'),
+    ]
+    
+    if os.path.exists(self.name):
+      shutil.rmtree(self.name)
+      
+  def tearDown (self):
+    if os.path.exists(self.name):
+      shutil.rmtree(self.name)
+      
+  def test_project (self):
+    pipe = subprocess.Popen(
+      ['djzen', 'startproject', self.name],
+      stdin=subprocess.PIPE,
+      stdout=subprocess.PIPE
+    )
+    stdout, stderr = pipe.communicate(input=b'2\n\n\n')
+    
+    for file in self.files:
+      path = os.path.join(self.name, file)
+      self.assertTrue(os.path.exists(path))
+      
