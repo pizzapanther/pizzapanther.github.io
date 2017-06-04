6bb106ac6f49fafe60c57bee80cf31f18e80607f
Paul Bailey <paul.m.bailey@gmail.com>
2017-06-04 17:01:57 +0000

    added gitignore and requirements setup

--- libs/djzen/djzen/cli.py
+++ libs/djzen/djzen/cli.py
@@ -5,6 +5,43 @@ import sys
 import click
 from django.core import management
 
+def write_ignore (directory):
+  gitignore = os.path.join(directory, '.gitignore')
+  
+  with open(gitignore, 'a') as fh:
+    fh.write('\n# Added by djzen\n')
+    fh.write('.env\n')
+    fh.write('db.sqlite3\n')
+    fh.write('# End djzen\n\n')
+    
+def write_requirements (directory):
+  from pip._vendor import pkg_resources
+  from pip._vendor.packaging.utils import canonicalize_name
+  
+  requirements = os.path.join(directory, 'requirements.txt')
+  lines = []
+  
+  for p in pkg_resources.working_set:
+    pname = canonicalize_name(p.project_name)
+    write = False
+    if pname.lower() in ['djzen', 'uwsgi', 'whitenoise']:
+      write = True
+      
+    elif pname.lower().startswith('django'):
+      write = True
+      
+    if write:
+      lines.append('{}=={}'.format(pname, p.version))
+      
+  if lines:
+    lines.sort()
+    with open(requirements, 'a') as fh:
+      fh.write('\n# Added by djzen\n')
+      for line in lines:
+        fh.write(line + '\n')
+        
+      fh.write('# End djzen\n\n')
+      
 @click.group()
 def zen_commands ():
   pass
@@ -35,6 +72,16 @@ def startproject (name, directory):
   
   management.execute_from_command_line(args)
   
+  if not directory:
+    directory = os.path.join(os.getcwd(), name)
+    
+  if os.path.exists(directory):
+    if click.confirm('Setup .gitignore?', default=True):
+      write_ignore(directory)
+      
+    if click.confirm('Setup requirements.txt?', default=True):
+      write_requirements(directory)
+      
 if __name__ == '__main__':
   zen_commands()
   
