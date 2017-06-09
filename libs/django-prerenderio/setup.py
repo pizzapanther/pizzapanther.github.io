19fc1da6bd17918015a594b0297f6f450003b941
Paul Bailey <paul.m.bailey@gmail.com>
2017-06-09 01:37:10 +0000

    updated info

--- libs/django-prerenderio/setup.py
+++ libs/django-prerenderio/setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 
 setup(
   name='django-prerenderio',
-  version='17.1.3',
+  version='17.6.1',
   license='MIT',
   description="Django middleware to render Single Page Applications via prerender.io",
   
   author="Paul Bailey",
-  author_email="paul.m.bailey@gmail.com",
-  url="https://github.com/pizzapanther/django-prerenderio",
+  author_email="paul@neutrondrive.com",
+  url="https://github.com/pizzapanther/pizzapanther.github.io/tree/master/libs/django-prerenderio",
   keywords='django prerender prerender.io seo spa angular react vuejs',
   
   packages=['django_prerenderio'],
