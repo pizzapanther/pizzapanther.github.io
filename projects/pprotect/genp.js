f9ade72d66e0b94aad70e6a7cbc5b4b46d2c3001
Paul Bailey <paul.m.bailey@gmail.com>
2017-10-22 20:39:48 -0500

    Update genp.js

--- projects/pprotect/genp.js
+++ projects/pprotect/genp.js
@@ -3,7 +3,7 @@ function genpass () {
   p = encodeURIComponent(btoa(p));
 
   var s = document.getElementById('stag');
-  s.innerText = '<script id="pprotect" src="http://www.pizzapanther.com/projects/pprotect/pprotect.js?p=' + p + '"></script>';
+  s.value = '<script id="pprotect" src="http://www.pizzapanther.com/projects/pprotect/pprotect.js?p=' + p + '"></script>';
 
   return false;
 }
