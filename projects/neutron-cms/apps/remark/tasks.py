0485984a944bf7685aad8b5d6fd30e4a9a2f5828
Paul Bailey <paul.m.bailey@gmail.com>
2018-05-03 21:21:50 -0500

    added termux talk

--- projects/neutron-cms/apps/remark/tasks.py
+++ projects/neutron-cms/apps/remark/tasks.py
@@ -7,25 +7,24 @@ def run ():
   base = os.path.join(os.environ['HOME'], 'code/pizzapanther.github.io/projects/personal-site')
   slide_template = 'slides.html'
   folder = 'talks'
-  file = 'pipenv.md'
+  file = 'termux.md'
   outdir = os.path.join(os.environ['HOME'], 'code/pizzapanther.github.io')
-  
+
   infile = os.path.join(base, folder, file)
-  
+
   print('Found: {}'.format(infile))
   with open(infile, 'r') as fh:
     md_content = fh.read()
-    
+
   template = Template(md_content)
   context = Context({})
   content = template.render(context)
-  
+
   rendered = render_to_string(slide_template, {'content': content})
-  
+
   outfile = file.replace('.md', '.html')
   outpath = os.path.join(outdir, folder, outfile)
   with open(outpath, 'w') as fh:
     fh.write(rendered)
-    
+
   print('Created: {}'.format(outpath))
-  
---
+++ projects/personal-site/talks/termux.md
@@ -0,0 +1,15 @@
+{% load static %}
+class: inverse, spaced
+layout: true
+background-image: url({% static "img/nac.svg" %})
+
+---
+
+class: middle, center
+
+# Exiting Vim On A Touch Screen
+
+## Making Android Linux Your Development Environment
+
+[bit.ly/pipenv-slides](http://bit.ly/pipenv-slides)
+
