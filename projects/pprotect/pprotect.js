d0845eaf749c3d3be8073a4bf3010e15579d913e
Paul Bailey <paul.m.bailey@gmail.com>
2019-05-02 01:37:11 +0000

    multipass

--- projects/pprotect/pprotect.js
+++ projects/pprotect/pprotect.js
@@ -6,25 +6,28 @@ document.write("<style>\
     visibility: visible;\
   }\
 </style>");
-               
+
 document.addEventListener("DOMContentLoaded", function() {
   var s = document.querySelector("#pprotect");
   var encoded = s.attributes.src.value;
-  var url = new URL(encoded, location.href)
-  var b64 = url.searchParams.get('p');
+  var url = new URL(encoded, location.href);
+  var b64s = url.searchParams.getAll('p');
   //console.log(atob(b64));
-  
-  if (b64) {
-    var password = localStorage.password;
-    while (1) {
-      if (password == atob(b64)) {
-        localStorage.password = password;
-        break;
-      }
-      password = prompt('Page Password?');
+
+
+  var password = localStorage.password;
+  var decoded = [];
+  b64s.forEach(function (b64) {
+    decoded.push(atob(b64));
+  });
+  while (1) {
+    if (decoded.indexOf(password) > -1) {
+      localStorage.password = password;
+      break;
     }
+    password = prompt('Page Password?');
   }
-  
+
   var d = document.querySelector('body');
   d.classList.add('active');
 });
